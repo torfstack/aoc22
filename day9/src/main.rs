999ceefe1738b91b2658485ee4985bb0869c64a7
torfstack <torfstack@gmail.com>
2022-12-22 12:14:30 +0100

    day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,9 +1,18 @@
 use std::io::BufRead;
 
 fn main() {
+    let example_lines = read_in_file_called_example_per_line();
+    let mut example_array = setup_array(LENGTH);
+    let mut example_heads = setup_heads(10);
+    simulate_rope(example_lines, &mut example_array, &mut example_heads);
+    let example_solution = count_entries_different_from_zero(&example_array);
+    //print_array(&example_array);
+    println!("Solution of example: {}", example_solution);
+
     let lines = read_in_file_called_input_per_line();
     let mut array = setup_array(LENGTH);
-    simulate_rope(lines, &mut array);
+    let mut heads = setup_heads(10);
+    simulate_rope(lines, &mut array, &mut heads);
     let solution = count_entries_different_from_zero(&array);
     println!("Solution of day 9: {}", solution);
 }
@@ -11,89 +20,80 @@ fn main() {
 fn print_array(array: &Vec<Vec<char>>) {
     array.iter()
         .for_each(|line| { 
-            line.iter().for_each(|&e| print!("{}", e));
+            line.iter().for_each(|&e| print!("{} ", e));
             println!();
         });
 }
 
-
+type Head = (i32, i32);
 // FUCK GLOBALS OMFG
 static LENGTH: i32 = 1000;
-static mut HEAD_POSITION: (i32, i32) = (LENGTH, LENGTH);
-static mut TAIL_POSITION: (i32, i32) = (LENGTH, LENGTH);
 
 fn count_entries_different_from_zero(array: &Vec<Vec<char>>) -> u32 {
     let mut score = 0;
     array.iter()
-        .for_each(|line| line.iter().for_each(|&e| if e != '.' { score += 1; } ));
+        .for_each(|line| line.iter().for_each(|&e| if e == '#' { score += 1; } ));
     score
 }
 
-fn simulate_rope(lines: Vec<String>, array: &mut Vec<Vec<char>>) {
+fn simulate_rope(lines: Vec<String>, array: &mut Vec<Vec<char>>, heads: &mut Vec<Head>) {
     for line in lines {
         let (direction, amount) = parse_step(line);
-        simulate_step(direction, amount, array);
+        simulate_step(direction, amount, array, heads);
     }
 }
 
-fn simulate_step(direction: String, amount: i32, array: &mut Vec<Vec<char>>) {
-    // move HEAD_POSITION
+fn simulate_step(direction: String, amount: i32, array: &mut Vec<Vec<char>>, heads: &mut Vec<Head>) {
     // println!("Simulating step into direction {} of amount {}", direction, amount);
-    unsafe {
+    for _ in 0..amount {
+        // println!("Doing the {}.th step", i+1);
+        let mut delta_x: i32 = 0;
+        let mut delta_y: i32 = 0;
         match direction.as_str() {
             "L" => {
-                HEAD_POSITION = (HEAD_POSITION.0 - amount, HEAD_POSITION.1);
-                let mut difference = ((HEAD_POSITION.0 - TAIL_POSITION.0).abs(), (HEAD_POSITION.1 - TAIL_POSITION.1));
-                if difference.0 > 1 {
-                    for _ in 0..difference.0 - 1 {
-                        TAIL_POSITION = (TAIL_POSITION.0 - 1, TAIL_POSITION.1 + difference.1);
-                        difference.1 = 0;
-                        array[TAIL_POSITION.0 as usize][TAIL_POSITION.1 as usize] = '#';
-                    }
-                }
+                delta_x = -1;
             },
             "R" => {
-                HEAD_POSITION = (HEAD_POSITION.0 + amount, HEAD_POSITION.1);
-                let mut difference = ((HEAD_POSITION.0 - TAIL_POSITION.0).abs(), (HEAD_POSITION.1 - TAIL_POSITION.1));
-                if difference.0 > 1 {
-                    for _ in 0..difference.0 - 1 {
-                        TAIL_POSITION = (TAIL_POSITION.0 + 1, TAIL_POSITION.1 + difference.1);
-                        difference.1 = 0;
-                        array[TAIL_POSITION.0 as usize][TAIL_POSITION.1 as usize] = '#';
-                    }
-                }
+                delta_x = 1;
             },
             "D" => {
-                HEAD_POSITION = (HEAD_POSITION.0, HEAD_POSITION.1 + amount);
-                let mut difference = ((HEAD_POSITION.0 - TAIL_POSITION.0), (HEAD_POSITION.1 - TAIL_POSITION.1).abs());
-                if difference.1 > 1 {
-                    for _ in 0..difference.1 - 1 {
-                        TAIL_POSITION = (TAIL_POSITION.0 + difference.0, TAIL_POSITION.1 + 1);
-                        difference.0 = 0;
-                        array[TAIL_POSITION.0 as usize][TAIL_POSITION.1 as usize] = '#';
-                    }
-                }
+                delta_y = 1;
             },
             "U" => {
-                HEAD_POSITION = (HEAD_POSITION.0, HEAD_POSITION.1 - amount);
-                let mut difference = ((HEAD_POSITION.0 - TAIL_POSITION.0), (HEAD_POSITION.1 - TAIL_POSITION.1).abs());
-                if difference.1 > 1 {
-                    for _ in 0..difference.1 - 1 {
-                        TAIL_POSITION = (TAIL_POSITION.0 + difference.0, TAIL_POSITION.1 - 1);
-                        difference.0 = 0;
-                        array[TAIL_POSITION.0 as usize][TAIL_POSITION.1 as usize] = '#';
-                    }
-                }
+                delta_y = -1;
             },
             _ => panic!()
         }
+        assert!(delta_x.abs() + delta_y.abs() <= 1);
+
+        heads[0] = (heads[0].0 + delta_x, heads[0].1 + delta_y);
+        for i in 1..heads.len() {
+            let differences = vec![(heads[i-1].0 - heads[i].0), (heads[i-1].1 - heads[i].1)];
+            let distance = differences.iter().map(|&d| if d < 0 { -d } else { d }).max().unwrap();
+            if distance > 1 {
+                assert!(distance <= 2);
+                heads[i] = (heads[i].0 + differences[0].signum(), heads[i].1 + differences[1].signum());
+            }
+        }
+        array[heads[heads.len()-1].0 as usize][heads[heads.len()-1].1 as usize] = '#';
     }
+    
+    //print_array(&array);
+    //println!("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
 }
 
 fn parse_step(step: String) -> (String, i32) {
     let direction = step.split(" ").next().unwrap().to_string();
     let amount = step.split(" ").last().unwrap().parse::<i32>().unwrap();
     (direction, amount)
+}
+
+fn setup_heads(length: i32) -> Vec<Head> {
+    let mut result = Vec::with_capacity(length as usize);
+    for _ in 0..length {
+        result.push((LENGTH, LENGTH));
+    }
+    result
 }
 
 fn setup_array(length: i32) -> Vec<Vec<char>> {
@@ -117,6 +117,6 @@ fn read_in_file_called_input_per_line() -> Vec<String> {
 }
 
 fn read_in_file_called_example_per_line() -> Vec<String> {
-    let file = std::fs::read("example_still").unwrap();
+    let file = std::fs::read("example_long").unwrap();
     file.lines().into_iter().map(|f| f.unwrap()).collect()
 }
